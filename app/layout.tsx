d771abfa8fedbc157191fc01de95bea1c8a7f503
James Esguerra <jamesesguerra025@gmail.com>
2026-05-04 19:11:07 +0800

    disable theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,9 +34,7 @@ export default function RootLayout({
         museoModerno.variable
       )}
     >
-      <body>
-        <ThemeProvider>{children}</ThemeProvider>
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
