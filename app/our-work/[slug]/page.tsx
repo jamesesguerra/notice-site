3c0ec0863b0d7bb9557d7cead029715ba22b4c02
James Esguerra <jamesesguerra025@gmail.com>
2026-05-04 23:11:36 +0800

    test min h screen

--- app/our-work/[slug]/page.tsx
+++ app/our-work/[slug]/page.tsx
@@ -14,7 +14,7 @@ export default async function WorkDetailPage({
   if (!item) return notFound()
 
   return (
-    <section className="mx-auto max-w-7xl pt-7 md:px-4 md:pb-20">
+    <section className="mx-auto min-h-screen max-w-7xl pt-7 md:px-4 md:pb-20">
       <div className="flex flex-col">
         {item.images.map((img) => (
           <Image
